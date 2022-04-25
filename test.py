86a13130309dd635f8c3e0bf1f9320c15211e1a8
Wang-Chien-Yu <104212445+Wang-Chien-Yu@users.noreply.github.com>
2022-04-25 16:16:32 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,34 @@
+
+#讀取檔案
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count +=1
+		#if count % 1000 == 0: # %求餘數
+			#print(len(data))
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) 
+	#將每筆留言的字數存起來到sum_len
+print('每筆留言平均長度為: ', sum_len/len(data))
+
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print('一共有', len(new), '筆留言小於100')
+
+good = []
+
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言含有good')		
